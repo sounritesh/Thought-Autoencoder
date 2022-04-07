d8b76b9af55836c5d18f5a4455b54088bbc5f10b
sounritesh <sounritesh@gmail.com>
2022-04-07 11:43:22 +0530

    extended embeddings 3D

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,8 +5,8 @@ import torch
 # BASE_MODEL = AutoModel.from_pretrained(BASE_MODEL_PATH)
 # BASE_TOKENIZER = AutoTokenizer.from_pretrained(BASE_MODEL_PATH)
 
-# INPUT_SIZE = 768*2 + 163 + 12
-INPUT_SIZE = 768*2
+INPUT_SIZE = 768*2 + 163 + 12
+# INPUT_SIZE = 768*2
 OUTPUT_SIZE = 3
 
 DEVICE = torch.device("cuda")
